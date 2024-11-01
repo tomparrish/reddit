56ef52b404e3a063e7434b04d4a7b3afd8c7fcb5
Tom Parrish <tomparrish@yahoo.com>
2024-10-31 20:36:18 -0700

    Create results.txt for elo-spfl.py

---
+++ results.txt for elo-spfl.py
@@ -0,0 +1,62 @@
+2024-08-03,Hearts,Rangers,0,0
+2024-08-03,Motherwell,Ross County, 0, 0
+2024-08-04,Celtic,Kilmarnock, 4, 0
+2024-08-04,St. Mirren,Hibernian, 3, 0
+2024-08-04,Dundee United,Dundee, 2, 2
+2024-08-05,St. Johnstone,Aberdeen, 1, 2
+2024-08-10,Dundee,Hearts, 3, 1
+2024-08-10,Ross County,Dundee United, 1, 1
+2024-08-10,Rangers,Motherwell, 2, 1
+2024-08-11,Kilmarnock,St. Johnstone, 0, 3
+2024-08-11,Aberdeen,St. Mirren, 3, 1
+2024-08-11,Hibernian,Celtic, 0, 2
+2024-08-24,Dundee United,St. Johnstone, 2, 0
+2024-08-24,Hibernian,Dundee, 2, 2
+2024-08-24,Rangers,Ross County, 6, 0
+2024-08-25,St. Mirren,Celtic, 0, 3
+2024-08-25,Aberdeen,Kilmarnock, 2, 0
+2024-08-25,Motherwell,Hearts, 3, 1
+2024-08-31,Dundee,St. Mirren, 2, 2
+2024-08-31,St. Johnstone,Motherwell, 1, 2
+2024-08-31,Ross County,Aberdeen, 0, 1
+2024-09-01,Celtic,Rangers, 3, 0
+2024-09-01,Hearts,Dundee United, 0, 1
+2024-09-01,Kilmarnock,Hibernian, 1, 1
+2024-09-14,Aberdeen,Motherwell, 2, 1
+2024-09-14,Celtic,Hearts, 2, 0
+2024-09-14,Hibernian,St. Johnstone, 2, 0
+2024-09-14,Ross County,Dundee, 2, 0
+2024-09-14,St. Mirren,Kilmarnock, 2, 2
+2024-09-15,Dundee United,Rangers, 0, 1
+2024-09-21,Ross County,St. Johnstone, 3, 3
+2024-09-21,St. Mirren,Hearts, 2, 1
+2024-09-28,Dundee,Aberdeen, 1, 2
+2024-09-28,Hearts,Ross County, 1, 1
+2024-09-28,Kilmarnock,Dundee United, 3, 3
+2024-09-28,Motherwell,St. Mirren, 2, 1
+2024-09-28,St. Johnstone,Celtic, 0, 6
+2024-09-29,Rangers,Hibernian, 1, 0
+2024-10-05,Dundee,Kilmarnock, 2, 3
+2024-10-05,Hibernian,Motherwell, 1, 2
+2024-10-05,St. Mirren,Dundee United, 0, 1
+2024-10-06,Ross County,Celtic, 1, 2
+2024-10-06,Aberdeen,Hearts, 3, 2
+2024-10-06,Rangers,St. Johnstone, 2, 0
+2024-10-19,Celtic,Aberdeen, 2, 2
+2024-10-19,Dundee United,Hibernian, 3, 2
+2024-10-19,Hearts,St. Mirren, 4, 0
+2024-10-19,Motherwell,Dundee, 0, 1
+2024-10-19,St. Johnstone,Ross County, 3, 0
+2024-10-20,Kilmarnock,Rangers, 1, 0
+2024-10-26,Dundee,St. Johnstone, 1, 2
+2024-10-26,Ross County,Kilmarnock, 2, 1
+2024-10-26,Aberdeen,Dundee United, 1, 0
+2024-10-27,Hibernian,Hearts, 1, 1
+2024-10-27,Motherwell,Celtic, 0, 3
+2024-10-27,Rangers,St. Mirren, 1, 0
+2024-10-30,Celtic,Dundee, 2, 0
+2024-10-30,Dundee United,Motherwell, 1, 2
+2024-10-30,Hearts,Kilmarnock, 1, 2
+2024-10-30,Ross County,Hibernian, 0, 0
+2024-10-30,St. Mirren,St. Johnstone, 3, 1
+2024-10-30,Aberdeen,Rangers, 2, 1
